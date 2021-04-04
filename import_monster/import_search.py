074c53ded85f4fcbc0517fce3169afee80ef3803
natalia <nata.a.vorobyeva@gmail.com>
2021-04-04 22:50:37 +0800

    Hello pre-commit

--- import_monster/import_search.py
+++ import_monster/import_search.py
@@ -14,16 +14,12 @@ def methods_importer(
             if isinstance(module, ModuleType):
                 mod = module
             elif isinstance(module, str):
-
-
                 mod = importlib.import_module(module)
             else:
                 raise TypeError("Must be list of strings or ModuleType")
             met = getattr(mod, method_name, None)
             if callable(met):
                 met_list.append(met)
-
         except ImportError:
             continue
-
     return met_list
